29ba0a8e40e54df29dcb27003e80bc57dea3c5f0
Emilien Klein <emilien@klein.st>
2020-08-28 13:37:16 +0200

    Argument to pass the input file

--- delimited2fixedwidth.py
+++ delimited2fixedwidth.py
@@ -6,11 +6,18 @@
 import sys
 import argparse
 import logging
+import os
 
 
 def parse_args(arguments):
     parser = argparse.ArgumentParser(description="Convert files from delimited "\
         "(e.g. CSV) to fixed width format")
+
+    parser.add_argument("-i", "--input",
+        help="Specify the input file",
+        action='store',
+        required=True
+    )
 
     parser.add_argument(
         '-d', '--debug',
@@ -29,6 +36,11 @@ def parse_args(arguments):
     if args.loglevel:
         logging.basicConfig(level=args.loglevel)
         args.logging_level = logging.getLevelName(args.loglevel)
+
+    # Validate if the arguments are used correctly
+    if not os.path.isfile(args.input):
+        logging.critical("The specified input file does not exist. Exiting...")
+        sys.exit(10)
 
     logging.debug("These are the parsed arguments:\n'%s'" % args)
     return args
