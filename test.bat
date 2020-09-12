9392ba38dc794696cf7d1c4f53bd952bcbe7d9d2
Emilien Klein <emilien@klein.st>
2020-09-12 08:01:28 +0200

    Add flake8 command to test launcher

--- test.bat
+++ test.bat
@@ -1,4 +1,5 @@
 @echo off
 coverage run --include=./*.py --omit=tests/*,.venv-delimited2fixedwidth/* -m unittest discover || EXIT /B 1
+flake8 delimited2fixedwidth.py --statistics --count || EXIT /B 1
 rd /s /q html_dev\coverage
 coverage html --directory=html_dev\coverage --title="Code test coverage for delimited2fixedwidth"
--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 coverage run --include=./*.py --omit=tests/*,.venv-delimited2fixedwidth/* -m unittest discover && \
+flake8 delimited2fixedwidth.py --statistics --count && \
 rm -rf html_dev/coverage && \
 coverage html --directory=html_dev/coverage --title="Code test coverage for delimited2fixedwidth"
